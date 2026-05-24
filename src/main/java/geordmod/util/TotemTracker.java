856d4f16b3096bb0cc6dc8a7b1d8fad00ec45bfe
Zerafyn <114658775+clainenxx@users.noreply.github.com>
2026-05-24 18:12:11 +0700

    Delete src/main directory

--- src/main/java/geordmod/util/TotemTracker.java
+++
@@ -1,9 +0,0 @@
-package geordmod.util;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-public class TotemTracker {
-    public static final Map<UUID, Integer> POPS = new HashMap<>();
-}
